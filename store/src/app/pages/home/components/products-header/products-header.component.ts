02026d5987c9ee8cc20ade04ff49480d8422e108
Martin Gallauner <martin.gallauner@prolion.com>
2023-02-26 11:13:41 +0100

    adds simple product header

--- store/src/app/pages/home/components/products-header/products-header.component.ts
+++ store/src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
-  templateUrl: 'products-header.html'
+  templateUrl: 'products-header.component.html'
 })
 export class ProductsHeaderComponent implements OnInit {
+  @Output() columnsCountChange = new EventEmitter<number>();
+
+  sort = "desc";
+  itemsCount = 12;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsCount = count;
+  }
+
+  onColumnsUpdated(columsNumber: number): void {
+    this.columnsCountChange.emit(columsNumber);
+  }
 }
