4b5ef221794848a97ef765bbd2d17ce369270295
Martin Gallauner <martin.gallauner@prolion.com>
2023-03-07 20:31:16 +0100

    Attached fakestoreapi

--- store/src/app/pages/home/home.component.ts
+++ store/src/app/pages/home/home.component.ts
@@ -1,42 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CartService} from "../../services/cart.service";
 import {Product} from "../../models/product.model";
+import {Subscription} from "rxjs";
+import {StoreService} from "../../services/store.service";
 
-const ROWS_HEIGHT: { [id:number]: number } = { 1: 400, 3: 335, 4: 350};
+const ROWS_HEIGHT: { [id: number]: number } = {1: 400, 3: 335, 4: 350};
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+    selector: 'app-home',
+    templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  cols = 3;
-  rowHeight = ROWS_HEIGHT[this.cols];
-  category: string | undefined;
+    cols = 3;
+    rowHeight = ROWS_HEIGHT[this.cols];
+    category: string | undefined;
+    products: Array<Product> | undefined;
+    sort = 'desc';
+    count = '12';
+    productsSubscription: Subscription | undefined;
 
-  constructor(private cartService: CartService) { }
+    constructor(private cartService: CartService, private storeService: StoreService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnDestroy(): void {
+        if (this.productsSubscription) {
+            this.productsSubscription.unsubscribe();
+        }
+    }
 
-  onColumnsCountChange(colsNum: number): void {
-    this.cols = colsNum;
-    this.rowHeight = ROWS_HEIGHT[this.cols];
-  }
+    ngOnInit(): void {
+        this.getProducts();
+    }
 
-  onShowCategory(newCategory: string): void {
-    this.category = newCategory;
-  }
+
+    onColumnsCountChange(colsNum: number): void {
+        this.cols = colsNum;
+        this.rowHeight = ROWS_HEIGHT[this.cols];
+    }
+
+    onShowCategory(newCategory: string): void {
+        this.category = newCategory;
+    }
 
     onAddToCart(product: Product): void {
-      this.cartService.addToCart({
-        product: product.image,
-        name: product.title,
-        price: product.price,
-        quantity: 1,
-        id: product.id
-      });
+        this.cartService.addToCart({
+            product: product.image,
+            name: product.title,
+            price: product.price,
+            quantity: 1,
+            id: product.id
+        });
 
+    }
+
+    private getProducts(): void {
+        this.productsSubscription = this.storeService.getAllProducts(this.count, this.sort)
+            .subscribe((_products) => {
+                this.products = _products;
+            });
+
+    }
+
+    onItemsCountChange(newCount: number): void {
+        this.count = newCount.toString();
+        this.getProducts();
+
+    }
+
+    onSortchange(newSort: string): void {
+        this.sort = newSort;
+        this.getProducts();
     }
 }
